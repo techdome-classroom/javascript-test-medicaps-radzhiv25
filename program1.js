7eb4e230e457a46f566e64315e01778ebafab416
radzhiv <radzhivkrishna@gmail.com>
2024-04-25 14:00:24 +0530

    Program completed

--- program1.js
+++ program1.js
@@ -1,7 +1,23 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+  // Implementation of longestSubstring function
+  let maxLength = 0; 
+  let charSet = new Set(); 
+  let left = 0; 
+
+  for (let right = 0; right < s.length; right++) {
+    if (charSet.has(s[right])) {
+      while (s[left] !== s[right]) {
+        charSet.delete(s[left]);
+        left++;
+      }
+      left++;
+    } else {
+      charSet.add(s[right]);
+    }
+    maxLength = Math.max(maxLength, charSet.size);
+  }
+
+  return maxLength;
 }
 
 module.exports = { longestSubstring };
-
-
--- program2.js
+++ program2.js
@@ -1,6 +1,30 @@
 function smallestMissingPositiveInteger(nums) {
 
   // Implement the function smallest_missing_positive_integer
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] <= 0 || nums[i] > n) {
+      nums[i] = 1;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    const a = Math.abs(nums[i]);
+    
+    if (a <= n) {
+      nums[a - 1] = -Math.abs(nums[a - 1]);
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) {
+      return i + 1;
+    }
+  }
+
+  
+  return n + 1;
 
 }
 
